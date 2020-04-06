b4c5de656e6fe962dad7fdddac2c6f92e53ce29e
LI, Qi <goodlq11@gmail.com>
2020-04-06 02:31:39 +0000

    123

--- proj.sh
+++ proj.sh
@@ -13,17 +13,10 @@ conda activate clvision-challenge
             --ewc_explosion_multr_cap=$ewc_explosion_multr_cap
     }
     function main_ewc_var_weight {
-        ewc_weight=5 main_ewc
-        ewc_weight=10 main_ewc
-        ewc_weight=20 main_ewc
-        ewc_weight=40 main_ewc
-        ewc_weight=50 main_ewc
-        ewc_weight=70 main_ewc
-        ewc_weight=100 main_ewc
+        ewc_weight=25 main_ewc
+        ewc_weight=30 main_ewc
+        ewc_weight=45 main_ewc
     }
     python main.py --scenario="multi-task-nc" --epochs="2" --sub_dir="baseline-$(date '+%m%d%H%M')"
-    ewc_explosion_multr_cap=5 main_ewc_var_weight
-    ewc_explosion_multr_cap=10 main_ewc_var_weight
     ewc_explosion_multr_cap=15 main_ewc_var_weight
-    ewc_explosion_multr_cap=20 main_ewc_var_weight
 } |& tee "proj-$(date '+%m%d%H%M').log" &)
